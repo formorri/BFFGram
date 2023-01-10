ca4977a0e585ea04da687345c4558cbd9ac3c15a
User <user@Users-iMac.local>
2023-01-10 16:19:19 +0800

    Troubleshooting nodemailer

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -5,6 +5,9 @@ const pass= process.env.NEXT_PUBLIC_EMAIL_PASS;
 
 export const transporter = nodemailer.createTransport({
     service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth:{
         user: email,
         pass,
