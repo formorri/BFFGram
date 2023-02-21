fca129d35484b4610d3f0d0bd4fcdb35247d5583
User <user@Users-iMac.local>
2023-02-21 18:49:23 +0800

    Trial 1

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import { React, useState, useMemo } from 'react';
 import styles from './Footer.module.scss';
-import Grid12 from '../Grid-1-2';
+// import Grid12 from '../Grid-1-2';
 import Image from 'next/image';
 
 import { Button } from '@mui/material';
@@ -124,7 +124,7 @@ const Footer = () => {
         />
       </div>
       <div className={styles.grid}>
-        <div className={styles.form}>
+      <div className={`${styles["form"]} ${styles["form-position"]}`}>
           <div className={styles['form__header']}>
             <h2>Get in touch</h2>
             <Image
@@ -134,177 +134,107 @@ const Footer = () => {
               loading="lazy"
             />
           </div>
-
-          {error && <p className={styles.error}>{error}</p>}
-          {submitted && (
-            <p className={styles.success}>Thanks for contacting us!</p>
-          )}
-
-          <form className={styles.field} onSubmit={handleContactFormSubmit}>
-            <FormControl
-            >
-              <Input
-                type='text'
-                name='name'
-                value={formState.name}
-                onChange={(e) =>
-                  setFormState({ ...formState, name: e.target.value })
-                }
-                placeholder="Enter your name"
-                required
-              />
-              <MyFormHelperText />
-            </FormControl>
-            <FormControl>
-              <Input
-                type='email'
-                name='email'
-                value={formState.email}
-                onChange={(e) =>
-                  setFormState({ ...formState, email: e.target.value })
-                }
-                placeholder="Enter your email"
-                required
-              />
-              <MyFormHelperText />
-            </FormControl>
-            <FormControl>
-              <Input
-                type='text'
-                name='message'
-                value={formState.message}
-                onChange={(e) =>
-                  setFormState({ ...formState, message: e.target.value })
-                }
-                placeholder="Drop us a message"
-                multiline
-                maxRows={4}
-              />
-            </FormControl>
-            <Button
-              className={styles.submit}
-              disabled={
-                !formState.name || !formState.email
-              }
-              onClick={handleContactFormSubmit}
-            >
-              <ArrowOutwardIcon />
-              Submit
-            </Button>
-          </form>
-          {/* <form className={styles.field} >
-            <FormControl
-            >
-              <Input
-                type='text'
-                name='name'
-                value={values.name}
-                onChange={handleChange}
-                placeholder="Enter your name"
-                required
-              />
-              <MyFormHelperText />
-            </FormControl>
-            <FormControl>
-              <Input
-                type='email'
-                name='email'
-                value={values.email}
-                onChange={handleChange}
-                placeholder="Enter your email"
-                required
-              />
-              <MyFormHelperText />
-            </FormControl>
-            <FormControl>
-              <Input
-                type='text'
-                name='message'
-                value={values.message}
-                onChange={handleChange}
-                placeholder="Drop us a message"
-                multiline
-                maxRows={4}
-              />
-            </FormControl>
-            <Button
-              className={styles.submit}
-              disabled={
-                !values.name || !values.email
-              }
-              onClick={onSubmit}
-            >
-              <ArrowOutwardIcon />
-              Submit
-            </Button>
-          </form> */}
-
-
+          <div className={styles.field}>
+            <Link href='mailto:contactus@bffgram.com'>
+              <Button
+                className={styles.submit}
+              >
+                <ArrowOutwardIcon />
+                Click here to email us
+              </Button>
+            </Link>
+          </div>
         </div>
-        <div className={styles.flex}>
-          <div className={styles.info}>
-            <div className={styles.info__info}>
-              <Grid12>
-                <Card className={styles.text}>
-                  <div>
-                    <h2>Sitemap</h2>
-                  </div>
-                  <ul>
-                    <li>
-                      <Link href='/'>
-                        Home
-                        <Image
-                          src={images.star}
-                          alt="star"
-                          className={styles['star']}
-                          loading="lazy"
-                        />
-                      </Link>
-                    </li>
-                    <li>
+        <div className={`${styles["info"]} ${styles["sitemap-position"]}`}>
+          <div className={styles.info__info}>
+            <Card className={styles.text}>
+              <div>
+                <h2>Sitemap</h2>
+              </div>
+              <ul>
+                <li>
+                  <Link href='/'>
+                    Home
+                    <Image
+                      src={images.star}
+                      alt="star"
+                      className={styles['star']}
+                      loading="lazy"
+                    />
+                  </Link>
+                </li>
+                <li>
 
-                      <Link href='/about'>
-                        About
-                        <Image
-                          src={images.star}
-                          alt="star"
-                          className={styles['star']}
-                          loading="lazy"
-                        />
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href='/tokenomics'>
-                        Tokenomics
-                        <Image
-                          src={images.star}
-                          alt="star"
-                          className={styles['star']}
-                          loading="lazy"
-                        />
-                      </Link>
-                    </li>
-                  </ul>
-                </Card>
-                <Card className={styles.text}>
-                  <div>
-                    <h2>Address</h2>
-                  </div>
-                  <ul>
-                    <li>
-                      Abu Dhabi, UAE
-                    </li>
-                  </ul>
-                </Card>
-              </Grid12>
-              <Card className={styles.text}>
-                <div>
-                  <h2>Email</h2>
-                </div>
-                <ul>
+                  <Link href='/about'>
+                    About
+                    <Image
+                      src={images.star}
+                      alt="star"
+                      className={styles['star']}
+                      loading="lazy"
+                    />
+                  </Link>
+                </li>
+                <li>
+                  <Link href='/tokenomics'>
+                    Tokenomics
+                    <Image
+                      src={images.star}
+                      alt="star"
+                      className={styles['star']}
+                      loading="lazy"
+                    />
+                  </Link>
+                </li>
+              </ul>
+            </Card>
+          </div>
+        </div>
+        <div className={`${styles["info"]} ${styles["address-position"]}`}>
+          <div className={styles.info__info}>
+            <Card className={styles.text}>
+              <div>
+                <h2>Address</h2>
+              </div>
+              <ul>
+                <li>
+                  Abu Dhabi, UAE
+                </li>
+              </ul>
+            </Card>
+          </div>
+        </div>
+        <div className={`${styles["info"]} ${styles["email-position"]}`}>
+          <div className={styles.info__info}>
+            <Card className={styles.text}>
+              <div>
+                <h2>Email</h2>
+              </div>
+              <ul>
+                <li>
+                  <Link href='mailto:contactus@bffgram.com' className={styles.email}>
+                    contactus@bffgram.com
+                    <Image
+                      src={images.star}
+                      alt="star"
+                      className={styles['star']}
+                      loading="lazy"
+                    />
+                  </Link>
+                </li>
+              </ul>
+            </Card>
+          </div>
+        </div>
+        <div className={`${styles["info"]} ${styles["contact-position"]}`}>
+          <div className={styles.info__info}>
+            <Card className={`${styles["text"]} ${styles["contacts"]}`}>
+              <div>
+                <h2>Telephone</h2>
+                <ul className={styles.tel}>
                   <li>
-                    <Link href='mailto:contactus@bffgram.com' className={styles.email}>
-                      contactus@bffgram.com
+                    <Link href='tel:+971507086658'>
+                      +971507086658
                       <Image
                         src={images.star}
                         alt="star"
@@ -314,64 +244,32 @@ const Footer = () => {
                     </Link>
                   </li>
                 </ul>
-              </Card>
-              <Card className={`${styles["text"]} ${styles["contacts"]}`}>
-                <div>
-                  <h2>Telephone</h2>
-                  <ul className={styles.tel}>
-                    <li>
-                      <Link href='tel:+971507086658'>
-                        +971507086658
-                        <Image
-                          src={images.star}
-                          alt="star"
-                          className={styles['star']}
-                          loading="lazy"
-                        />
-                      </Link>
-                    </li>
-                  </ul>
-                  {/* <p>+971507086658</p> */}
-                </div>
+                {/* <p>+971507086658</p> */}
+              </div>
 
-                <ul>
-                  <li>
-                    <Link href='https://www.instagram.com/invites/contact/?i=v777t1mecqqp&utm_content=76l7sbn'>
-                      <InstagramIcon></InstagramIcon>
-                    </Link>
-                  </li>
-                  {/* <li>
-                    <Link href='/'>
-                      <FacebookIcon></FacebookIcon>
-                    </Link>
-                  </li> */}
-                  <li>
-                    <Link href='https://www.linkedin.com/in/monahaljneibi'>
-                      <LinkedInIcon></LinkedInIcon>
-                    </Link>
-                  </li>
-                  {/* <li>
-                    <Link href='/'>
-                      <TwitterIcon></TwitterIcon>
-                    </Link>
-                  </li> */}
-                  {/* <li>
-                    <Link href='/'>
-                      <YouTubeIcon></YouTubeIcon>
-                    </Link>
-                  </li> */}
-                </ul>
-              </Card>
-            </div>
-            <div className={styles['back-to-top']}>
-              <Link href='#home'>
-                <div className={styles.scroll}>
-                  <ArrowUpwardIcon />
-                  <p>Go back to top</p>
-                </div>
-              </Link>
-            </div>
+              <ul>
+                <li>
+                  <Link href='https://www.instagram.com/invites/contact/?i=v777t1mecqqp&utm_content=76l7sbn'>
+                    <InstagramIcon></InstagramIcon>
+                  </Link>
+                </li>
+
+                <li>
+                  <Link href='https://www.linkedin.com/in/monahaljneibi'>
+                    <LinkedInIcon></LinkedInIcon>
+                  </Link>
+                </li>
+              </ul>
+            </Card>
           </div>
+        </div>
+        <div className={`${styles["back-to-top"]} ${styles["top-position"]}`}>
+          <Link href='#home'>
+            <div className={styles.scroll}>
+              <ArrowUpwardIcon />
+              <p>Go back to top</p>
+            </div>
+          </Link>
         </div>
       </div>
     </footer>
