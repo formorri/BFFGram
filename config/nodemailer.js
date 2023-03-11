8975ce404c395fa70b2e529f898048f6fe6dd18f
Lam Zi Xin <lamzixin@Lams-Mac-mini.local>
2023-03-11 20:34:42 +0800

    trial 1

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,20 +1,20 @@
-import nodemailer from "nodemailer"
+// import nodemailer from "nodemailer"
 
-const email = process.env.NEXT_PUBLIC_EMAIL;
-const pass = process.env.NEXT_PUBLIC_EMAIL_PASS;
+// const email = process.env.NEXT_PUBLIC_EMAIL;
+// const pass = process.env.NEXT_PUBLIC_EMAIL_PASS;
 
-export const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: email,
-        pass,
-    }
-})
+// export const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     host: 'smtp.gmail.com',
+//     port: 465,
+//     secure: true,
+//     auth: {
+//         user: email,
+//         pass,
+//     }
+// })
 
-export const mailOptions = {
-    from: email,
-    to: email,
-}
+// export const mailOptions = {
+//     from: email,
+//     to: email,
+// }
